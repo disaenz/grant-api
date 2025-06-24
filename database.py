e91e639b5841679ce0c3afc15985ac5f0aeb582c
Daniel Saenz <disaenz2@gmail.com>
2025-06-23 23:29:23 -0600

    Added support to use .env files and configured database.py to be able to use neondb connection_string

--- database.py
+++ database.py
@@ -1,11 +1,16 @@
 import os
+import ssl
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://myuser:mypassword@localhost:5432/mydb")
+raw_url = os.getenv("DATABASE_URL", "postgresql+asyncpg://myuser:mypassword@localhost:5432/mydb")
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+DATABASE_URL = raw_url.split("?", 1)[0]
+
+ssl_ctx = ssl.create_default_context()
+
+engine = create_async_engine(DATABASE_URL, echo=True, connect_args={"ssl": ssl_ctx})
 
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
