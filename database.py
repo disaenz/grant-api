513b3b81a2bc85c2694cec9c6bd982bd9c4f45f8
Daniel Saenz <disaenz2@gmail.com>
2025-06-29 12:41:26 -0600

    allowing support for localhost test for DATABASE_URL and also for db service name when using Docker Compose

--- database.py
+++ database.py
@@ -1,19 +1,26 @@
 import os
 import ssl
+from urllib.parse import urlparse
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 raw_url = os.getenv("DATABASE_URL", "postgresql+asyncpg://myuser:mypassword@localhost:5432/mydb")
-
 DATABASE_URL = raw_url.split("?", 1)[0]
 
-ssl_ctx = ssl.create_default_context()
+# Parse the DB URL to extract the hostname
+parsed_url = urlparse(DATABASE_URL)
+hostname = parsed_url.hostname
 
-engine = create_async_engine(DATABASE_URL, echo=True, connect_args={"ssl": ssl_ctx})
+# Disable SSL for localhost or Docker Compose service name "db"
+if hostname in ("localhost", "127.0.0.1", "db"):
+    connect_args = {}
+else:
+    ssl_ctx = ssl.create_default_context()
+    connect_args = {"ssl": ssl_ctx}
 
+engine = create_async_engine(DATABASE_URL, echo=True, connect_args=connect_args)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
 Base = declarative_base()
 
 # Dependency for async sessions
